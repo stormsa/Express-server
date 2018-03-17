132caed0becf40d56a943a2f0b31c2e9a688bb77
stormsa <sofiane.aziri@gmail.com>
2018-03-17 13:58:18 +0100

    Ajout d'une base de donnée Mongo DB

--- src/serveur/serveur.js
+++ src/serveur/serveur.js
@@ -5,11 +5,29 @@ var url = require("url");
 var querystring = require('querystring');
 var express = require('express');
 var bodyParser = require('body-parser')
+var MongoClient  = require("mongodb").MongoClient;
+var MongoObjectID = require("mongodb").ObjectID;
 var urlencodedParser = bodyParser.urlencoded({extended:false})
 var app = express();
 
 app.use(express.static(__dirname+'./../../'))
 
+MongoClient.connect("mongodb://localhost/dashboard", function(error, db) {
+	if (error) throw error;
+	console.log("Connecté à la base de donnée dashboard")
+	db.collection("plants").find().toArray(function(error, results){
+		if(error) throw error;
+		var plants = []
+		results.forEach(function(i, obj){
+			var plant = {
+				id : obj._id.toString(),
+				nom: obj.name,
+				lastArrosage: obj.lastArrosage,
+				instructions : obj.instructions
+			}
+		}
+	}
+}
 // create application/json parser 
 var jsonParser = bodyParser.json()
  
@@ -19,10 +37,35 @@ console.log("hello ")
 require('./response')
 
 app.get('/plant', function(req, res){
-  console.log("Get last arrosage for the plant")
+	var plantId = req.body.plant_id;
+	var plantToFind = {_id : new MongoObjectID(idToFind)};
+	db.collection("plants").findOne(objToFind, function(error, result){
+		if(error) throw error
+		var plant = {
+			id : obj._id.toString(),
+			nom: obj.name,
+			lastArrosage: obj.lastArrosage,
+			instructions : obj.instructions		
+		}
+	}
+	res.respond(plant, 200)
 });
 app.post('/plant', function(req, res){
-  console.log("save plant")
+	var newPlant = {
+			nom: req.body.name,
+			lastArrosage: req.body.lastArrosage,
+			instructions : req.body.instructions		
+		}
+	db.collection("plants").insert(newPlant, function(error, result){
+		if(error) throw error
+		 console.log("save plant")
+	}
+	response = {
+		message: "Votre plante a bien été sauvegardée",
+		nextArrosage: "15 jours"
+	}
+	res.respond(response, 200)
+ 
 });
 app.put('/arrosePlant', urlencodedParser, function(req, res){
   if (!req.body) return res.sendStatus(400)
